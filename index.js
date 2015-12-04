0c381b4970196dd521fc6fc95223b50a7fd4c8fe
Tulio Monte Azul <tuliomonteazul@gmail.com>
2015-12-04 20:12:12 -0200

    First runnable version

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var express = require('express');
+var app = express();
+var host = '0.0.0.0' || process.env.NODE_HOST;
+var port = 3000 || process.env.NODE_PORT;
+
+// app.get('/', function (req, res) {
+//   res.send('ae');
+// });
+
+app.use(express.static('public'));
+app.use(express.static('bower_components'));
+
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
